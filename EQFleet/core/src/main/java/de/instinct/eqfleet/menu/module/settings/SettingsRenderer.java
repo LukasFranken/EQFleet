0db7941dc7cceaa1013a5a43756bac6b701b12bf
LukasFranken <m16a1@live.de>
2025-06-14 12:18:22 +0200

    small settings feature copy key

--- EQFleet/core/src/main/java/de/instinct/eqfleet/menu/module/settings/SettingsRenderer.java
+++ EQFleet/core/src/main/java/de/instinct/eqfleet/menu/module/settings/SettingsRenderer.java
@@ -1,58 +1,63 @@
 package de.instinct.eqfleet.menu.module.settings;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Rectangle;
 
 import de.instinct.api.core.API;
 import de.instinct.eqfleet.menu.common.architecture.BaseModuleRenderer;
+import de.instinct.eqfleet.menu.common.components.DefaultButtonFactory;
+import de.instinct.eqlibgdxutils.ClipboardUtil;
 import de.instinct.eqlibgdxutils.PreferenceUtil;
+import de.instinct.eqlibgdxutils.debug.logging.ConsoleColor;
+import de.instinct.eqlibgdxutils.debug.logging.Logger;
 import de.instinct.eqlibgdxutils.generic.Action;
-import de.instinct.eqlibgdxutils.rendering.ui.DefaultUIValues;
 import de.instinct.eqlibgdxutils.rendering.ui.component.active.button.ColorButton;
 import de.instinct.eqlibgdxutils.rendering.ui.component.passive.label.Label;
-import de.instinct.eqlibgdxutils.rendering.ui.core.Border;
 
 public class SettingsRenderer extends BaseModuleRenderer {
 	
 	private ColorButton resetTokenButton;
+	private ColorButton copyTokenButton;
 	private Label authKeyLabel;
 	
 	public SettingsRenderer() {
-		Border buttonBorder = new Border();
-		buttonBorder.setSize(2);
-		buttonBorder.setColor(Color.GRAY);
-		
-		resetTokenButton = new ColorButton("Delete Key");
-		resetTokenButton.setBorder(buttonBorder);
-		resetTokenButton.setColor(Color.BLACK);
-		resetTokenButton.setFixedWidth(120);
-		resetTokenButton.setFixedHeight(30);
-		resetTokenButton.setLabelColor(new Color(DefaultUIValues.skinColor));
-		resetTokenButton.setHoverColor(new Color(DefaultUIValues.darkerSkinColor));
-		resetTokenButton.setDownColor(new Color(DefaultUIValues.lighterSkinColor));
-		resetTokenButton.setActive(true);
-		resetTokenButton.setAction(new Action() {
+		resetTokenButton = DefaultButtonFactory.colorButton("Delete", new Action() {
 			
 			@Override
 			public void execute() {
 				PreferenceUtil.save("authkey", "");
 				API.authKey = "";
-				System.out.println("Authkey deleted!");
+				Logger.log("Settings", "Authkey deleted!", ConsoleColor.YELLOW);
 			}
 			
 		});
-		resetTokenButton.setBorder(buttonBorder);
+		resetTokenButton.setFixedWidth(80);
+		resetTokenButton.setFixedHeight(30);
+		
+		copyTokenButton = DefaultButtonFactory.colorButton("Copy", new Action() {
+			
+			@Override
+			public void execute() {
+				ClipboardUtil.setContent(API.authKey);
+				Logger.log("Settings", "Authkey copied!", ConsoleColor.YELLOW);
+			}
+			
+		});
+		copyTokenButton.setFixedWidth(80);
+		copyTokenButton.setFixedHeight(30);
 		
 		authKeyLabel = new Label("");
 	}
 
 	@Override
 	public void render() {
-		resetTokenButton.setPosition(Gdx.graphics.getWidth() / 2 - 60, Gdx.graphics.getHeight() / 2);
-		resetTokenButton.render();
-		
 		if (API.authKey != null) {
+			resetTokenButton.setPosition(Gdx.graphics.getWidth() / 2 - 90, Gdx.graphics.getHeight() / 2);
+			resetTokenButton.render();
+			
+			copyTokenButton.setPosition(Gdx.graphics.getWidth() / 2 + 10, Gdx.graphics.getHeight() / 2);
+			copyTokenButton.render();
+			
 			authKeyLabel.setBounds(new Rectangle(0, Gdx.graphics.getHeight() / 2 - 50, Gdx.graphics.getWidth(), 30));
 			authKeyLabel.setText(API.authKey);
 			authKeyLabel.render();
