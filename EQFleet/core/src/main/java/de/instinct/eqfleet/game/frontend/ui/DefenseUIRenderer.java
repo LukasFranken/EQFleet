01cca194b79bf566653ad6dff0acc6bd6c27cab3
Instinct <m16a1@live.de>
2025-06-17 15:27:37 +0200

    Update DefenseUIRenderer.java
    no more overlapping ship bars

--- EQFleet/core/src/main/java/de/instinct/eqfleet/game/frontend/ui/DefenseUIRenderer.java
+++ EQFleet/core/src/main/java/de/instinct/eqfleet/game/frontend/ui/DefenseUIRenderer.java
@@ -1,5 +1,8 @@
 package de.instinct.eqfleet.game.frontend.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
@@ -9,6 +12,7 @@ import com.badlogic.gdx.math.Vector3;
 import de.instinct.engine.combat.Ship;
 import de.instinct.engine.model.GameState;
 import de.instinct.engine.model.planet.Planet;
+import de.instinct.engine.model.ship.Defense;
 import de.instinct.eqlibgdxutils.rendering.ui.component.passive.loadingbar.types.rectangular.Direction;
 import de.instinct.eqlibgdxutils.rendering.ui.component.passive.loadingbar.types.rectangular.subtypes.PlainRectangularLoadingBar;
 import de.instinct.eqlibgdxutils.rendering.ui.texture.TextureManager;
@@ -32,6 +36,7 @@ public class DefenseUIRenderer {
 	public void render(GameState state, PerspectiveCamera camera) {
 		for (Planet planet : state.planets) {
 			if (planet.defense != null) {
+				Defense defense = planet.defense.clone();
 				Vector3 screenPos = camera.project(new Vector3(planet.position.x, planet.position.y, 0f));
 				TextureManager.draw("ui_defense_border_planet", 
 						new Rectangle(screenPos.x - 20 - 100, 
@@ -42,40 +47,69 @@ public class DefenseUIRenderer {
 				
 				PlainRectangularLoadingBar armorBar = createBar(Color.ORANGE);
 				armorBar.setBounds(new Rectangle(screenPos.x - 19, screenPos.y - 2, 38, 4));
-				armorBar.setCurrentValue(planet.defense.currentArmor);
-				armorBar.setMaxValue(planet.defense.armor);
+				armorBar.setCurrentValue(defense.currentArmor);
+				armorBar.setMaxValue(defense.armor);
 				armorBar.render();
 				
 				PlainRectangularLoadingBar shieldBar = createBar(new Color(0, 0.8f, 0.8f, 1f));
 				shieldBar.setBounds(new Rectangle(screenPos.x - 19, screenPos.y - 2, 38, 4));
-				shieldBar.setCurrentValue(planet.defense.currentShield);
-				shieldBar.setMaxValue(planet.defense.shield);
+				shieldBar.setCurrentValue(defense.currentShield);
+				shieldBar.setMaxValue(defense.shield);
 				shieldBar.render();
 			}
 		}
-		for (Ship ship : state.ships) {
-			if (ship.defense != null) {
-				Vector3 screenPos = camera.project(new Vector3(ship.position.x, ship.position.y, 0f));
-				TextureManager.draw("ui_defense_border_ship", 
-						new Rectangle(screenPos.x - 10 - 100, 
-								screenPos.y - 17 - 100, 
-								Gdx.graphics.getWidth(),
-								Gdx.graphics.getHeight()),
-						1f);
-				
-				PlainRectangularLoadingBar armorBar = createBar(Color.ORANGE);
-				armorBar.setBounds(new Rectangle(screenPos.x - 9, screenPos.y - 16, 18, 2));
-				armorBar.setCurrentValue(ship.defense.currentArmor);
-				armorBar.setMaxValue(ship.defense.armor);
-				armorBar.render();
-				
-				PlainRectangularLoadingBar shieldBar = createBar(new Color(0, 0.8f, 0.8f, 1f));
-				shieldBar.setBounds(new Rectangle(screenPos.x - 9, screenPos.y - 16, 18, 2));
-				shieldBar.setCurrentValue(ship.defense.currentShield);
-				shieldBar.setMaxValue(ship.defense.shield);
-				shieldBar.render();
-			}
-		}
+		List<Rectangle> occupiedAreas = new ArrayList<>();
+
+	    for (Ship ship : state.ships) {
+	        if (ship.defense != null) {
+	        	Defense defense = ship.defense.clone();
+	            Vector3 screenPos = camera.project(new Vector3(ship.position.x, ship.position.y, 0f));
+
+	            // Default position and size of defense bar area (including a small buffer)
+	            Rectangle defenseArea = new Rectangle(screenPos.x - 10, screenPos.y - 17, 20, 4);
+	            float originalY = defenseArea.y;
+
+	            // Find the lowest overlapping bar
+	            float maxBottom = originalY;
+	            for (Rectangle occupied : occupiedAreas) {
+	                if (defenseArea.overlaps(occupied)) {
+	                    float occupiedBottom = occupied.y;
+	                    maxBottom = Math.min(maxBottom, occupiedBottom - 4); // 4 pixels for height + small spacing
+	                }
+	            }
+
+	            // If we found an overlap, adjust y position
+	            float yOffset = 0;
+	            if (maxBottom < originalY) {
+	                yOffset = maxBottom - originalY;
+	                defenseArea.y = maxBottom;
+	            }
+
+	            // Add this area to occupied areas
+	            occupiedAreas.add(new Rectangle(defenseArea));
+
+	            // Draw UI with calculated offset
+	            TextureManager.draw("ui_defense_border_ship",
+	                    new Rectangle(screenPos.x - 10 - 100,
+	                            screenPos.y - 17 - 100 + yOffset,
+	                            Gdx.graphics.getWidth(),
+	                            Gdx.graphics.getHeight()),
+	                    1f);
+
+	            // Both bars remain overlapped for each individual ship
+	            PlainRectangularLoadingBar armorBar = createBar(Color.ORANGE);
+	            armorBar.setBounds(new Rectangle(screenPos.x - 9, screenPos.y - 16 + yOffset, 18, 2));
+	            armorBar.setCurrentValue(defense.currentArmor);
+	            armorBar.setMaxValue(defense.armor);
+	            armorBar.render();
+
+	            PlainRectangularLoadingBar shieldBar = createBar(new Color(0, 0.8f, 0.8f, 1f));
+	            shieldBar.setBounds(new Rectangle(screenPos.x - 9, screenPos.y - 16 + yOffset, 18, 2));
+	            shieldBar.setCurrentValue(defense.currentShield);
+	            shieldBar.setMaxValue(defense.shield);
+	            shieldBar.render();
+	        }
+	    }
 	}
 
 	private PlainRectangularLoadingBar createBar(Color color) {
