12f041a6db30c906693996d07ba53f124bbf51f3
Instinct <m16a1@live.de>
2026-04-09 09:16:03 +0200

    minig

--- EQFleet/core/src/main/java/de/instinct/eqfleet/mining/Mining.java
+++ EQFleet/core/src/main/java/de/instinct/eqfleet/mining/Mining.java
@@ -9,6 +9,8 @@ import de.instinct.eqlibgdxutils.rendering.ui.core.Border;
 public class Mining extends Scene {
 	
 	private BoxedRectangularLoadingBar bar;
+	
+	private ShipRenderer shipRenderer;
 
 	@Override
 	public void init() {
@@ -19,6 +21,9 @@ public class Mining extends Scene {
 		bar = new BoxedRectangularLoadingBar();
 		bar.setFixedHeight(10f);
 		bar.setBorder(barBorder);
+		
+		shipRenderer = new ShipRenderer();
+		shipRenderer.init();
 	}
 
 	@Override
@@ -37,15 +42,19 @@ public class Mining extends Scene {
 		bar.setMaxValue(10);
 		bar.setCurrentValue(10);
 		bar.setBounds(20, GraphicsUtil.screenBounds().height - 20, 160, 6);
+		
+		shipRenderer.update();
 	}
 
 	@Override
 	public void render() {
+		shipRenderer.render();
 		bar.render();
 	}
 
 	@Override
 	public void dispose() {
+		shipRenderer.dispose();
 		bar.dispose();
 	}
 
--- EQFleet/core/src/main/java/de/instinct/eqfleet/mining/ShipRenderer.java
+++ EQFleet/core/src/main/java/de/instinct/eqfleet/mining/ShipRenderer.java
@@ -1,6 +1,11 @@
+
 package de.instinct.eqfleet.mining;
 
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+
 public class ShipRenderer {
+	
+	private ModelInstance model;
 	
 	public void init() {
 		
