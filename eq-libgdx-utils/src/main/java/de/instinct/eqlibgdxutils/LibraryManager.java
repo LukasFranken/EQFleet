0d258c554f0daed4e9aaf5247085fae76397ee32
LukasFranken <m16a1@live.de>
2026-07-26 17:12:15 +0200

    libman

---
+++ eq-libgdx-utils/src/main/java/de/instinct/eqlibgdxutils/LibraryManager.java
@@ -0,0 +1,41 @@
+package de.instinct.eqlibgdxutils;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.math.Vector2;
+
+import de.instinct.eqlibgdxutils.debug.console.Console;
+import de.instinct.eqlibgdxutils.engine.cursor.CursorUtil;
+import de.instinct.eqlibgdxutils.engine.cursor.Hotspot;
+import de.instinct.eqlibgdxutils.rendering.ui.font.FontUtil;
+import de.instinct.eqlibgdxutils.rendering.ui.popup.PopupRenderer;
+import de.instinct.eqlibgdxutils.rendering.ui.skin.SkinManager;
+import de.instinct.eqlibgdxutils.rendering.ui.texture.shape.Shapes;
+
+public class LibraryManager {
+	
+	public static void initialize(Vector2 windowSize) {
+		Console.init();
+		PreferenceUtil.init("Equilite");
+		GraphicsUtil.init(windowSize);
+		Gdx.input.setInputProcessor(new InputMultiplexer());
+		PopupRenderer.init();
+		FontUtil.init();
+		Shapes.init();
+    	SkinManager.init();
+    	CursorUtil.setCursor("cursor", Hotspot.TOPLEFT);
+        Console.build();
+        AccelerometerUtil.init();
+	}
+	
+	public static void update() {
+		InputUtil.update();
+		AccelerometerUtil.update();
+	}
+	
+	public static void dispose() {
+        Console.dispose();
+        Shapes.dispose();
+	}
+
+}
